70f13b1f1e993b5e03bd551d65dfb9878b5f197a
nvidia <billdalton2@gmail.com>
2021-09-05 15:29:18 -0400

    added minion monitors

--- src/node_monitor_nx_node.cpp
+++ src/node_monitor_nx_node.cpp
@@ -7,8 +7,11 @@
 #include <std_msgs/Int16.h>
 #include <std_msgs/String.h>
 #include <time.h>		/* ROS time functions */
+#include <geometry_msgs/Vector3.h>
+#include <my_robotic_arm_nx/MinionState.h>
 
 //variables
+//************ VARIABLES ****************//
 int minion_controller_last_counter = 0;
 int pick_place_last_counter = 0;
 int my_node_detectnet_last_counter = 0;
@@ -19,11 +22,19 @@ bool pick_place_good = false;
 bool my_node_detectnet_good = false;
 bool fiducial_service_good = false;
 bool finder_service_good = false;
+bool BR_minion_good = false;
+bool SL_minion_good = false;
+//bool UR_minion_good = false;
+bool EL_minion_good = false;
+bool LR_minion_good = false;
+bool WL_minion_good = false;
+bool WR_minion_good = false;
 bool verbose = false;
 
 //subscribers
-ros::Subscriber minion_controller_sub;
+//ros::Subscriber minion_controller_sub;
 
+//************ NODE MONITORS ****************//
 void minionControllerRecdCallback(const std_msgs::Int16& minion_controller_msg_){
 	if (minion_controller_msg_.data > minion_controller_last_counter) {
 		minion_controller_good = true;
@@ -69,6 +80,31 @@ void finderServiceRecdCallback(const std_msgs::Int16& finder_service_msg_){
 	}
 }//end finderServiceRecdCallback()
 
+//************ MINION STATE MONITORS ****************//
+//receiving a message from a minion shows that minion is still operating
+void BRMinionStateRecdCallback(const my_robotic_arm_nx::MinionState& minion_state_msg_){
+	BR_minion_good = true;
+}
+void SLMinionStateRecdCallback(const my_robotic_arm_nx::MinionState& minion_state_msg_){
+	SL_minion_good = true;
+}
+//void URMinionStateRecdCallback(const my_robotic_arm_nx::MinionState& minion_state_msg_){
+	//UR_minion_good = true;
+//}
+void ELMinionStateRecdCallback(const my_robotic_arm_nx::MinionState& minion_state_msg_){
+	EL_minion_good = true;
+}
+void LRMinionStateRecdCallback(const my_robotic_arm_nx::MinionState& minion_state_msg_){
+	LR_minion_good = true;
+}
+void WLMinionStateRecdCallback(const geometry_msgs::Vector3& minion_Vector3_state_msg_){
+	WL_minion_good = true;
+}
+void WRMinionStateRecdCallback(const geometry_msgs::Vector3& minion_Vector3_state_msg_){
+	WR_minion_good = true;
+}
+
+//************ MAIN ****************//
 int main(int argc, char* argv[]){	
 	//vars
 	char verbose_cmd[] = "verbose";   //verbose command line arg
@@ -94,6 +130,13 @@ int main(int argc, char* argv[]){
 	ros::Subscriber my_node_detectnet_sub = nh.subscribe("my_node_detectnet_watchdog", 1, myNodeDetectnetRecdCallback);
 	ros::Subscriber fiducial_service_sub = nh.subscribe("fiducial_service_watchdog", 1, fiducialServiceRecdCallback);
 	ros::Subscriber finder_service_sub = nh.subscribe("finder_service_watchdog", 1, finderServiceRecdCallback);
+	ros::Subscriber BR_minion_state_sub = nh.subscribe("BR_minion_state", 1000, BRMinionStateRecdCallback);
+	ros::Subscriber SL_minion_state_sub = nh.subscribe("SL_minion_state", 1000, SLMinionStateRecdCallback);
+	//ros::Subscriber UR_minion_state_sub = nh.subscribe("UR_minion_state", 1000, URMinionStateRecdCallback);
+	ros::Subscriber EL_minion_state_sub = nh.subscribe("EL_minion_state", 1000, ELMinionStateRecdCallback);
+	ros::Subscriber LR_minion_state_sub = nh.subscribe("LR_minion_state", 1000, LRMinionStateRecdCallback);
+	ros::Subscriber WL_minion_state_sub = nh.subscribe("WL_joint_state", 1000, WLMinionStateRecdCallback);//note xx_joint_state for Dynamixel versions versus xx_minion_state for Teensy versions
+	ros::Subscriber WR_minion_state_sub = nh.subscribe("WR_joint_state", 1000, WRMinionStateRecdCallback);
 
 	while (nh.ok()){
 		//check to see if nodes are good
@@ -134,6 +177,48 @@ int main(int argc, char* argv[]){
 		} else {
 			std::cout << "finder_service          FAIL" << std::endl;
 		}			
+	//******	
+		if (BR_minion_good) {
+			std::cout << "BR_minion	  GOOD" << std::endl;
+		} else {
+			std::cout << "BR_minion	        FAIL" << std::endl;
+		}			
+		
+		if (SL_minion_good) {
+			std::cout << "SL_minion	  GOOD" << std::endl;
+		} else {
+			std::cout << "SL_minion	        FAIL" << std::endl;
+		}			
+		
+		//if (UR_minion_good) {
+			//std::cout << "UR_minion	  GOOD" << std::endl;
+		//} else {
+			//std::cout << "UR_minion	        FAIL" << std::endl;
+		//}			
+		
+		if (EL_minion_good) {
+			std::cout << "EL_minion	  GOOD" << std::endl;
+		} else {
+			std::cout << "EL_minion	        FAIL" << std::endl;
+		}			
+		
+		if (LR_minion_good) {
+			std::cout << "LR_minion	  GOOD" << std::endl;
+		} else {
+			std::cout << "LR_minion	        FAIL" << std::endl;
+		}			
+		
+		if (WL_minion_good) {
+			std::cout << "WL_minion	  GOOD" << std::endl;
+		} else {
+			std::cout << "WL_minion	        FAIL" << std::endl;
+		}			
+		
+		if (WR_minion_good) {
+			std::cout << "WR_minion	  GOOD" << std::endl;
+		} else {
+			std::cout << "WR_minion	        FAIL" << std::endl;
+		}			
 		
 		//ouput a blank line for clearer formating
 		std::cout << " " << std::endl;
@@ -144,6 +229,13 @@ int main(int argc, char* argv[]){
 		my_node_detectnet_good = false;
 		fiducial_service_good = false;
 		finder_service_good = false;
+		BR_minion_good = false;
+		SL_minion_good = false;
+		//UR_minion_good = false;
+		EL_minion_good = false;
+		LR_minion_good = false;
+		WL_minion_good = false;
+		WR_minion_good = false;
 		
 		//spin and sleep
 		ros::spinOnce();
